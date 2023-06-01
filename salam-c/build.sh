3f402545a26c5c2c7b69f9abbc5bb345935748b8
ngm <ngmaibulat@gmail.com>
2023-06-01 10:02:21 +0300

    build script updated

--- salam-c/build.sh
+++ salam-c/build.sh
@@ -2,18 +2,24 @@
 
 rm -fr build
 mkdir build
-cd build
 
-cmake ..
+cmake -B ./build
 
-make
-
+cmake --build ./build
 
 os=$(uname)
 
 # Check if the OS is not Darwin
 if [ "$os" != "Darwin" ]; then
+    mkdir snapcraft-build
+    mv build/salam-c snapcraft-build
+    cp snapcraft-dump.yaml snapcraft-build/snapcraft.yaml
+    cd snapcraft-build
     snapcraft
+    snapcraft upload  salam-c_*_amd64.snap
 else
-    echo "MacOS is detected..."
+    echo -e "\nMacOS is detected...\n"
 fi
+
+rm -fr build
+rm -fr snapcraft-build
